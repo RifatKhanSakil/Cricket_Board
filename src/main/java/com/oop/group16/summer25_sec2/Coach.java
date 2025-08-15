fc05978f1b5ddd59c614d7fb5b47f57f3383ea26
JakiyaAnnanJoly <2221057@iub.edu.bd>
2025-08-15 23:19:12 +0600

    Coach to CoachAssignTrainingSession, CoachFitnessReview, CoachStrategicPlanningView scene switching

--- src/main/java/com/oop/group16/summer25_sec2/Coach.java
+++ src/main/java/com/oop/group16/summer25_sec2/Coach.java
@@ -3,29 +3,24 @@ package com.oop.group16.summer25_sec2;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 public class Coach
 {
     @javafx.fxml.FXML
+    private Button FitnessReviewButton;
+    @javafx.fxml.FXML
+    private Button StrategicPlanningButton;
+    @javafx.fxml.FXML
+    private Button LogOutButton;
+    @javafx.fxml.FXML
+    private Button AssignTrainingSessionButton;
+    @javafx.fxml.FXML
+    private Button viewPlayerReportsButton;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }
-
-    @javafx.fxml.FXML
-    public void MonitorPlayerPerformanceOnAction(ActionEvent actionEvent) {
-    }
-
-    @javafx.fxml.FXML
-    public void ScheduleTrainingOnAction(ActionEvent actionEvent) {
-    }
-
-    @javafx.fxml.FXML
-    public void SendFeedbackToPlayersOnAction(ActionEvent actionEvent) {
-    }
-
-    @Deprecated
-    public void ReviewPlayerReportsOnAction(ActionEvent actionEvent) {
-
     }
 
     @javafx.fxml.FXML
@@ -44,13 +39,55 @@ public class Coach
     }
 
     @javafx.fxml.FXML
-    public void MessageTeamManagerOnAction(ActionEvent actionEvent) {
-    }
-
-    @javafx.fxml.FXML
     public void viewPlayerReportsOnAction(ActionEvent actionEvent) {
         try{
             FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("CoachViewPlayerReports.fxml"));
+            Scene scene = new Scene(fxmlLoader.load());
+            Stage stage=new Stage();
+            stage.setTitle("Hello!");
+            stage.setScene(scene);
+            stage.show();
+        }
+        catch(Exception e) {
+            System.out.println(" ");
+        }
+    }
+
+    @javafx.fxml.FXML
+    public void AssignTrainingSessionOnAction(ActionEvent actionEvent) {
+        try{
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("CoachAssignTrainingSession.fxml"));
+            Scene scene = new Scene(fxmlLoader.load());
+            Stage stage=new Stage();
+            stage.setTitle("Hello!");
+            stage.setScene(scene);
+            stage.show();
+        }
+        catch(Exception e) {
+            System.out.println(" ");
+        }
+
+    }
+
+    @javafx.fxml.FXML
+    public void FitnessReviewOnAction(ActionEvent actionEvent) {
+        try{
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("CoachFitnessReview.fxml"));
+            Scene scene = new Scene(fxmlLoader.load());
+            Stage stage=new Stage();
+            stage.setTitle("Hello!");
+            stage.setScene(scene);
+            stage.show();
+        }
+        catch(Exception e) {
+            System.out.println(" ");
+        }
+    }
+
+    @javafx.fxml.FXML
+    public void StrategicPlanningOnAction(ActionEvent actionEvent) {
+        try{
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("CoachStrategicPlanningView.fxml"));
             Scene scene = new Scene(fxmlLoader.load());
             Stage stage=new Stage();
             stage.setTitle("Hello!");
---
+++ src/main/java/com/oop/group16/summer25_sec2/CoachStrategicPlanningViewController.java
@@ -0,0 +1,45 @@
+package com.oop.group16.summer25_sec2;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.stage.Stage;
+
+public class CoachStrategicPlanningViewController
+{
+    @javafx.fxml.FXML
+    private Label titleLabel;
+    @javafx.fxml.FXML
+    private TextArea battingOrderTextArea;
+    @javafx.fxml.FXML
+    private TextArea bowlingPlanTextArea;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void returnOnAction(ActionEvent actionEvent) {
+        try{
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Coach.fxml"));
+            Scene scene = new Scene(fxmlLoader.load());
+            Stage stage=new Stage();
+            stage.setTitle("Hello!");
+            stage.setScene(scene);
+            stage.show();
+        }
+        catch(Exception e) {
+            System.out.println(" ");
+        }
+    }
+
+    @javafx.fxml.FXML
+    public void printPlanOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void savePlanOnAction(ActionEvent actionEvent) {
+    }
+}
