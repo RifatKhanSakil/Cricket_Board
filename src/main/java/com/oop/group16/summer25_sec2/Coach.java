2ec51fb1ecd2c1b4ac0c2265ae46db10b05206fa
JakiyaAnnanJoly <2221057@iub.edu.bd>
2025-08-15 20:53:08 +0600

    Coach to ViewPlayerReports scene switching n scene switching

--- src/main/java/com/oop/group16/summer25_sec2/Coach.java
+++ src/main/java/com/oop/group16/summer25_sec2/Coach.java
@@ -1,6 +1,9 @@
 package com.oop.group16.summer25_sec2;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class Coach
 {
@@ -20,15 +23,42 @@ public class Coach
     public void SendFeedbackToPlayersOnAction(ActionEvent actionEvent) {
     }
 
-    @javafx.fxml.FXML
+    @Deprecated
     public void ReviewPlayerReportsOnAction(ActionEvent actionEvent) {
+
     }
 
     @javafx.fxml.FXML
     public void LogOutOnAction(ActionEvent actionEvent) {
+        try{
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Login.fxml"));
+            Scene scene = new Scene(fxmlLoader.load());
+            Stage stage=new Stage();
+            stage.setTitle("Hello!");
+            stage.setScene(scene);
+            stage.show();
+        }
+        catch(Exception e) {
+            System.out.println(" ");
+        }
     }
 
     @javafx.fxml.FXML
     public void MessageTeamManagerOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void viewPlayerReportsOnAction(ActionEvent actionEvent) {
+        try{
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("CoachViewPlayerReports.fxml"));
+            Scene scene = new Scene(fxmlLoader.load());
+            Stage stage=new Stage();
+            stage.setTitle("Hello!");
+            stage.setScene(scene);
+            stage.show();
+        }
+        catch(Exception e) {
+            System.out.println(" ");
+        }
     }
 }
---
+++ src/main/java/com/oop/group16/summer25_sec2/CoachViewPlayerReportsController.java
@@ -0,0 +1,51 @@
+package com.oop.group16.summer25_sec2;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+
+public class CoachViewPlayerReportsController
+{
+    @javafx.fxml.FXML
+    private TableView reportTable;
+    @javafx.fxml.FXML
+    private ComboBox playerNameComboBox;
+    @javafx.fxml.FXML
+    private Label titleLabel;
+    @javafx.fxml.FXML
+    private TableColumn additionalInformationColumn;
+    @javafx.fxml.FXML
+    private TableColumn valueColumn;
+    @javafx.fxml.FXML
+    private ComboBox matchTypeComboBox;
+    @javafx.fxml.FXML
+    private Button loadReportButton;
+    @javafx.fxml.FXML
+    private TableColumn statTypeColumn;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void returnOnAction(ActionEvent actionEvent) {
+        try{
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Coach.fxml"));
+            Scene scene = new Scene(fxmlLoader.load());
+            Stage stage=new Stage();
+            stage.setTitle("Hello!");
+            stage.setScene(scene);
+            stage.show();
+        }
+        catch(Exception e) {
+            System.out.println(" ");
+        }
+
+    }
+
+    @javafx.fxml.FXML
+    public void loadReportOnAction(ActionEvent actionEvent) {
+    }
+}
